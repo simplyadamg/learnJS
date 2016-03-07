54dd0480c4fdab9d1c64149f76fc50b73fd5672a
Adam <simplyadamg@gmail.com>
2016-03-07 11:13:32 -0600

    Crane logo updated, snap SVG library added, charcoal and off white colors

--- weirdParts.js
+++ weirdParts.js
@@ -1,15 +1,85 @@
-Address of a website often has the following form: <protocol>://<domain>.com[/<context>] where <protocol>, <domain> and <context> are non-empty strings consisting of lowercase English letters.
-Note that there may be no <context> part in an address.
+// function greet(firstname, lastname, language){
+//     language = language || 'en';
 
-We need an algorithm that splits an address into an array of its parts: <protocol>, <domain> and <context> (if there is one).
+//     if (language === 'en'){
+//         console.log('Hello ' + firstname + ' ' + lastname)
+//     }
 
-Example
+//     if (language === 'es'){
+//         console.log('Hola ' + firstname + ' ' + lastname)
+//     }
 
-splitAddress('http://codefights.com') = ['http', 'codefights']
-splitAddress('http://stackoverflow.com/questions') = ['http', 'stackoverflow', 'questions']
-[input] string address
 
-valid website address
-[output] array.string
+// }
 
-Address elements <protocol>, <domain> and <context> placed in the given order.
+// function greetEnglish(firstname,lastname){
+//     greet(firstname, lastname, 'en')
+// }
+
+// function greetSpanish(firstname, lastname){
+//     greet(firstname, lastname, 'es')
+// }
+// greetEnglish('John', 'Doe');
+// greetSpanish('John', 'Doe')
+
+
+// // Syntax parser inserts semicolon after return, this is a
+// // problem with automatic semicolon insertion
+// function getPerson(){
+//     return
+//     {
+//         firstname:"tony"
+//     }
+// }
+// console.log(getPerson());
+// //Helpful to always put curley brackets directly after the end of function.
+
+function rot13(str) { // LBH QVQ VG!
+  return str;
+}
+
+// Change the inputs below to test
+// rot13("SERR PBQR PNZC");
+// arr=[1,2,3]
+// x = arr[0]
+// y = arr[1]
+// z = arr[2]
+// console.log(String.fromCharCode(x, y, z))
+
+// Faking Namespaces
+// Container for vars and functions JS doesn't use these because of the nature of objects in JS
+
+//First class functions
+//JS uses First class functions which call be passed around, created on the fly and generally used in the same way sas other types. 
+//Can be anonymous, or without a name.
+//Code: are where the actual lines of code sit.
+//The function is an object with many properties, code is one of those properties but it is not the funciton. 
+//code is invokeable function()
+
+//Function statment does work... A function expression creates a value that is not necessarly saved to a variable. 
+//Mutate - to change something and if it's immuitable that means it can't be changed
+function theTimeNow(){
+var dateString = "The time right now is: ";
+
+var newDate = new Date();
+
+// Get the month, day, and year.
+
+dateString += newDate.getHours() + ":";
+dateString += newDate.getMinutes();
+
+return dateString;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
